580f26d1a05fae4a6349e86e45365b8fb683e77b
drl990114 <drl990114@gmail.com>
2024-01-01 22:14:00 +0800

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# image load error
+
+- mac build: image load success
+- mac dev: image load error
+- windows build: image load error
+- windows dev: image load error
---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+</script>
+
+<template>
+  <div class="container">
+    <h1>Welcome to Tauri!</h1>
+
+    <div class="row">
+      <img src="https://i2.kknews.cc/SIG=1of3siu/5qr00060rss39243533.jpg" class="logo vite" alt="can't load" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.logo.vite:hover {
+  filter: drop-shadow(0 0 2em #747bff);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #249b73);
+}
+</style>
